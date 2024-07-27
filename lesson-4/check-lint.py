43942d8359f6ba920ab9c01de4e3dd1b9dbaf77c
pradiptadas1988 <pradiptadas.1988@hotmail.com>
2024-07-28 01:05:22 +0530

    lint UT error handling and initial practices (#4)
    Co-authored-by: PraddyBorg <43722656+PraddyBorg@users.noreply.github.com>

---
+++ lesson-4/check-lint.py
@@ -0,0 +1,14 @@
+'''
+A simple example
+'''
+
+def myfunction():
+    '''
+    A simple add
+    '''
+    first_value = 2
+    second_value = 8
+
+    print(first_value + second_value)
+
+myfunction()
---
+++ lesson-4/unit-test-sample/cap.py
@@ -0,0 +1,25 @@
+'''
+A Simple method to capitalise
+Case one
+'''
+
+'''
+def capitaliseFunc(user_input):
+    
+    # Make first letter capital
+    
+    return user_input.capitalize()
+'''
+
+'''
+A Simple method to capitalise
+Case two
+'''
+
+def capitaliseFunc(user_input):
+    '''
+    Make first letter capital
+    of all word
+    '''
+
+    return user_input.title()
